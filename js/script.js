433bc1041fc1b9417bb5e20088869c9b4144b0d3
gohanxl <lucasmlacquaniti@gmail.com>
2019-11-22 01:24:24 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var elem = document.querySelector(".sidenav");
+var instance = new M.Sidenav(elem);
+
+
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
+//por que con js vanilla no anda?
+
+  
